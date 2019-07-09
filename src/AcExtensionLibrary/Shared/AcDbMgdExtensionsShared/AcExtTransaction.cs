fd41e2a8b6f54d7cf5ee834d73337b5f55fe8b22
Jeff Hornsby <hpadgroup@outlook.com>
2019-07-09 00:26:05 -0500

    added docfx

---
+++ src/AcExtensionLibrary/Shared/AcDbMgdExtensionsShared/AcExtTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autodesk.AutoCAD
+{
+   public class AcExtTransaction: IDisposable
+
+    {
+       private AcExtTransaction()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
